cd89dc5a3c1ea3da82c86113dcdabb83d3aa5c85
Mohamad Kassem <moha226i@edu.zealand.dk>
2023-11-30 12:52:30 +0100

    har tilføjet startup.cs til at håntere login (blot en ide) kan ændres

---
+++ Fdfvanlose/startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Konfigurer tjenester her, f.eks. autentificering, autorisation osv.
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            // Konfigurer fejlhåndtering i produktionsmiljøet
+        }
+
+        // Konfigurer middleware og rutehåndtering her
+    }
+}
